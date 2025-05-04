a56867edc420e0c1d973519f02e59ed296fcd157
javier <javiercastillo.tuc@gmail.com>
2025-05-04 20:51:21 -0300

    se agrega DOM a ejercicio 1

--- Ejercicio 1/js/js.js
+++ Ejercicio 1/js/js.js
@@ -1,25 +1,38 @@
 //Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
 
+const estadoAuto = document.getElementById("estadoAuto")
+
+estadoAuto.textContent = "El auto esta apagado"
+
 const auto = {
-    marca: "ford",
-    modelo: "ecosport",
-    color: "azul",
+    marca: "Ford",
+    modelo: "Ecosport",
+    color: "Azul",
     estado: false,
     encenderAuto: function(){
         if (this.estado === false){
             this.estado = true
-            document.writeln(`<p>El auto se encendio</p>`)
+            btnEncender.disabled = true
+            btnApagar.disabled = false
+            estadoAuto.textContent = `El auto: ${auto.marca} ${auto.modelo} ${auto.color} se encendio.`
         }   
     },
     apagarAuto: function(){
         if (this.estado === true){
             this.estado = false
-            document.writeln(`<p>El auto se apago</p>`)
+            btnApagar.disabled = true
+            btnEncender.disabled = false
+            estadoAuto.textContent = estadoAuto.textContent = `El auto: ${auto.marca} ${auto.modelo} ${auto.color} se apago.`
+
         }   
     }
 }
+const btnEncender = document.getElementById("btnEncender")
+const btnApagar = document.getElementById("btnApagar")
+btnApagar.disabled = true
 
-auto.encenderAuto()
-auto.apagarAuto()
+btnEncender.addEventListener("click", ()=> auto.encenderAuto())
+btnApagar.addEventListener("click", ()=> auto.apagarAuto())
+
 
 
